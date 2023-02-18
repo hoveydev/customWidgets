d7ee06e8c0db1e4396b3f0d026a381f9fa8289b1
Robert Hovey <robert.hovey95@gmail.com>
2023-02-18 15:22:31 -0500

    initialize repo

---
+++ CustomWidgets/CustomWidgetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomWidgetsApp.swift
+//  CustomWidgets
+//
+//  Created by Robert Alec Hovey on 2/18/23.
+//
+
+import SwiftUI
+
+@main
+struct CustomWidgetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
